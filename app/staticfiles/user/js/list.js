f96d9792cc5150110c302e0205a6c517dbb85d0a
Isela <infantefernandezisela@gmail.com>
2021-08-27 20:49:23 -0400

    nine

--- app/staticfiles/user/js/list.js
+++ app/staticfiles/user/js/list.js
@@ -29,8 +29,6 @@ function getData() {
             $('#data').removeClass('dataTables_filter');
         },
         columns: [
-            { "data": "groups" },
-            { "data": "date_joined" },
             { "data": "first_name" },
             { "data": "last_name" },
             { "data": "image" },
